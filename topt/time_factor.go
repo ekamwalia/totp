3a59baf4b9a1099b4714867a410377efccd2fb36
Ekam Walia <ekamwalia@gmail.com>
2018-10-19 15:55:46 +0530

    Create time_factor.go

---
+++ topt/time_factor.go
@@ -0,0 +1,28 @@
+package totp
+
+import (
+  "math"
+  "time"
+)
+
+const (
+  time_step = 30
+  time0  = "1970-01-01T00:00:00Z"
+)
+
+func SetTimeStep(step int) {
+  time_step = step
+}
+
+func SetEpochTime(epoch string) {
+  time0 = epoch
+}
+
+func CalculateTimeSteps(curr_time_str string) float64 {
+  epoch_time, _ := time.Parse(time.RFC3339, time0)
+  curr_time, _ := time.Parse(time.RFC3339, curr_time_str)
+  duration := curr_time.Sub(epoch_time)
+  
+  T := math.Floor(duration.Seconds()/time_step)
+  return T
+}
